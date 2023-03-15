d79dc9a5ee0907286bfab199d5627d58deafe48a
Iaemacs <127951159+Iaemacs@users.noreply.github.com>
2023-03-15 09:06:35 -0300

    Opção de saída
    Inclusão da opção de saída

--- Cartorio.c
+++ Cartorio.c
@@ -162,6 +162,7 @@ int main ()
 		printf("\t1 - Registrar Nomes\n");
 		printf("\t2 - Consultar Nomes\n");
 		printf("\t3 - Deletar Nomes\n\n"); 
+		printf("\t4 - Sair do sistema\n\n");
 		printf("Opção: "); // Fim do menu
 	
 		scanf("%d" , &opcao);  //armazenar na variável, escolha do usuario
@@ -182,6 +183,11 @@ int main ()
 			case 3: 
 			deletar();
 			break;
+			
+			case 4:
+			Printf("Obrigado por utilizar o sistema!\n");
+			return 0;
+			break;	
 			
 			default: 
 			printf("Essa opção não está disponivel\n");
